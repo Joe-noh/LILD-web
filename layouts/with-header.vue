2c031d11a3ce969ae781287533251a58b5b2decd
Joe-noh <goflb.jh@gmail.com>
2019-04-06 01:38:13 +0900

    ちょっと遊んだ

---
+++ layouts/with-header.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app light>
+    <v-navigation-drawer v-model="drawer" temporary app>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>apps</v-icon>
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar dense app>
+      <v-toolbar-side-icon @click="drawer = !drawer" />
+    </v-toolbar>
+    <v-content>
+      <nuxt />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false
+    }
+  }
+}
+</script>
