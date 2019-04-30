c49d9e95e1216dc45b9c06d3e593a75db9453c30
Joe-noh <goflb.jh@gmail.com>
2019-04-30 22:04:32 +0900

    ついでにfavicon設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'Dream sharing SNS.' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/png', href: '/favicon.png' },
       {
         rel: 'stylesheet',
         href: 'https://fonts.googleapis.com/css?family=Material+Icons'
